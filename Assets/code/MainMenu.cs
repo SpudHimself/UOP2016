949ae5998cd0dbec61716dd78d5106c34d2452c9
floorman <tmp24054@uni.ds.port.ac.uk>
2016-06-07 11:23:19 +0100

    Added controller input to main menu

--- Assets/code/MainMenu.cs
+++ Assets/code/MainMenu.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
+using System.Collections.Generic;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
+    public Button startButton;
+    public Button creditsButton;
+    public Button helpButton;
+    public Button quitButton;
+    public Button currentButton;
+    public EventSystem eventSystem;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+    }
 
     public void LoadLevel(string filename)
     {
@@ -21,5 +32,60 @@ public class MainMenu : MonoBehaviour {
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void CheckInput()
+    {
+        if (currentButton == startButton)
+        {
+            if ((Input.GetButton("down")))
+            {
+                helpButton.Select();
+            }
+
+            if ((Input.GetButton("up")))
+            {
+                quitButton.Select();
+            }
+        }
+
+        else if (currentButton == helpButton)
+        {
+            if ((Input.GetButton("down")))
+            {
+                creditsButton.Select();
+            }
+
+            if ((Input.GetButton("up")))
+            {
+                startButton.Select();
+            }
+        }
+
+        else if (currentButton == creditsButton)
+        {
+            if ((Input.GetButton("down")))
+            {
+                quitButton.Select();
+            }
+
+            if ((Input.GetButton("up")))
+            {
+                helpButton.Select();
+            }
+        }
+
+        else if (currentButton == quitButton)
+        {
+            if ((Input.GetButton("down")))
+            {
+                startButton.Select();
+            }
+
+            if ((Input.GetButton("up")))
+            {
+                creditsButton.Select();
+            }
+        }
     }
 }
