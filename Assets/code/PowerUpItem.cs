c67d153f2e6d36d54e2ea5f47725e1d8b10fa46e
tmp24053 <tmp24053@uni.ds.port.ac.uk>
2016-06-07 09:46:07 +0100

    Changes to car level resetting.

---
+++ Assets/code/PowerUpItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ePowerUpType
+{
+    SPEED_BOOST
+}
+
+public class PowerUpItem : MonoBehaviour
+{
+    #region Fields
+    public ePowerUpType type;
+    #endregion
+
+    #region Unity Methods
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag.Equals(Tags.PLAYER))
+        {
+            switch (type)
+            {
+                case ePowerUpType.SPEED_BOOST:
+                    break;
+            }
+        }
+    }
+    #endregion
+
+    #region Methods
+    #endregion
+}
