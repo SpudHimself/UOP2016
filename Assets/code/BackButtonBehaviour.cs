7cb6a88e9782504e573c35413b66adcbad8bdb73
floorman <tmp24054@uni.ds.port.ac.uk>
2016-06-07 11:40:55 +0100

    Added controller input to back button and fixed input errors

--- Assets/code/BackButtonBehaviour.cs
+++ Assets/code/BackButtonBehaviour.cs
@@ -4,33 +4,24 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 
-public class MainMenu : MonoBehaviour
+public class BackButtonBehaviour : MonoBehaviour
 {
-    public Button startButton;
-    public Button creditsButton;
-    public Button helpButton;
-    public Button quitButton;
     public Button currentButton;
     public EventSystem eventSystem;
 
     // Use this for initialization
     void Start()
     {
+        currentButton.Select();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //CheckInput();
     }
 
     public void LoadLevel(string filename)
     {
         Application.LoadLevel(filename);
-    }
-
-    public void Quit()
-    {
-        Application.Quit();
     }
 }
