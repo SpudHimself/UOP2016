0cf29c94ea21eda58f7fa0735b78ad11d389d6aa
floorman <tmp24054@uni.ds.port.ac.uk>
2016-06-09 11:01:13 +0100

    Collision script done

---
+++ Assets/code/CollisionSounds.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionSounds : MonoBehaviour
+{
+    private GameObject bush;
+    public AudioSource audioSource;
+    public AudioClip bushCollision;
+    public AudioClip shelfCollision;
+    public AudioClip cartCollision;
+    public AudioClip boxCollision;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.relativeVelocity.magnitude > 2)
+        {
+            if (col.collider.tag == "Plant")
+            {
+                audioSource.clip = bushCollision;
+                audioSource.Play();
+            }
+
+            if (col.collider.tag == "Shelf")
+            {
+                audioSource.clip = shelfCollision;
+                audioSource.Play();
+            }
+
+            if (col.collider.tag == "Cart")
+            {
+                audioSource.clip = cartCollision;
+                audioSource.Play();
+            }
+
+            if (col.collider.tag == "Box")
+            {
+                audioSource.clip = boxCollision;
+                audioSource.Play();
+            }
+        }
+    }
+}
