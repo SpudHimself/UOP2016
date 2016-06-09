450b9e74c38558f71cc9e9e0528a39c73dc5707b
floorman <tmp24054@uni.ds.port.ac.uk>
2016-06-09 15:13:11 +0100

    Car has skidmarks, currently in the wrong position and texture BUT IT WORKS DAMMIT ACHOO

---
+++ Assets/code/WheelTrailRender.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelTrailRender : MonoBehaviour
+{
+    public WheelCollider mCorrespondingCollider;
+    public GameObject mSkidmarkTrailRenderer;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        mSkidmarkTrailRenderer.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        Vector3 ColliderCenterPoint = mCorrespondingCollider.transform.TransformPoint(mCorrespondingCollider.center);
+
+        if (Physics.Raycast(ColliderCenterPoint, -mCorrespondingCollider.transform.up, out hit, mCorrespondingCollider.suspensionDistance + mCorrespondingCollider.radius))
+        {
+            //transform.position = hit.point + (mCorrespondingCollider.transform.up * mCorrespondingCollider.radius);
+        }
+        else
+        {
+            //transform.position = ColliderCenterPoint - (mCorrespondingCollider.transform.up * mCorrespondingCollider.suspensionDistance);
+        }
+
+        WheelHit correspondingGroundHit;
+        mCorrespondingCollider.GetGroundHit(out correspondingGroundHit);
+
+        if (Mathf.Abs(correspondingGroundHit.sidewaysSlip) > 0.6f)
+        {
+            mSkidmarkTrailRenderer.gameObject.SetActive(true);
+        }
+        else if (Mathf.Abs(correspondingGroundHit.sidewaysSlip) <= 0.55f)
+        {
+            mSkidmarkTrailRenderer.gameObject.SetActive(false);
+        }
+            
+    }
+}
